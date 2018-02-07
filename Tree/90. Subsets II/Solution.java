1f43e1b8212632367487f12e49e4d3f1b0c2f4ae
kyun <839530159@qq.com>
2018-02-07 11:02:07 +0800

    递归回溯练习

---
+++ Tree/90. Subsets II/Solution.java
@@ -0,0 +1,49 @@
+/*
+ 时间复杂度 O(n^n)，n为数组的长度
+ 空间复杂度 O(2^n)
+
+ 算法思想：递归回溯，1、通过树形深度递归调用，遍历每种可能，把每次的结果添加到结果集中  2、当 start == nums.length 时，表示遍历到底，返回 
+ 3、在第一次调用方法时会将空的子结果集放入结果集中 4、判断之后的数字是否一样，如果一样，就直接跳过（相当于剪枝）
+*/
+
+
+class Solution {
+    
+    private ArrayList<List<Integer>> res;
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+        res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0){
+            return res;
+        }
+        
+        Arrays.sort(nums);
+        LinkedList<Integer> c = new LinkedList<Integer>();
+        sub_subsets(nums, 0, c);
+        
+        return res;
+    }
+    
+    private void sub_subsets(int[] nums, int start, LinkedList<Integer> c){
+        
+        res.add((List<Integer>)c.clone());
+        
+        if(start == nums.length){
+            return;
+        }
+        
+        for(int i=start; i<nums.length; i++){
+            
+            c.addLast(nums[i]);
+            sub_subsets(nums, i+1, c);
+            c.removeLast();
+            
+            while(i<nums.length-1 && nums[i] == nums[i+1]){
+                i++;
+            }
+        }
+        
+        return;
+    }
+}
